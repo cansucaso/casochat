baad49afd3590c81cb5f5cb41b7638ebfcc84974
Cansu Uludağ <cansu@artistanbulpr.com>
2014-10-22 17:39:55 +0300

    server mod

--- casochat/server.js
+++ casochat/server.js
@@ -12,3 +12,18 @@ http.createServer(function (request, response) {
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
+
+buf = new Buffer(256);
+len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
+var buf = new Buffer('test');
+var json = JSON.stringify(buf);
+
+console.log(json);
+// '[116,101,115,116]'
+
+var copy = new Buffer(JSON.parse(json)); // to hex
+
+console.log(copy);
+// <Buffer 74 65 73 74>
