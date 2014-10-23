9785364e42cbb1d8299155cbaa9552abc952359c
Cansu Uludağ <cansu@artistanbulpr.com>
2014-10-23 17:21:02 +0300

    chat complete

--- casochat/index.js
+++ casochat/index.js
@@ -1,10 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile('/Users/caso/Desktop/caso_chat/index.html');
+  console.log('selam');
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+io.on('connection', function(socket){
+	console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
