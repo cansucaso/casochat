57d004ade7d05ddddb3c248d9f49a48f53f5d1f8
Cansu Uludağ <cansu@artistanbulpr.com>
2014-10-30 16:57:51 +0200

    little changes on indentation

--- casochat/index.js
+++ casochat/index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('/Users/caso/Desktop/caso_chat/index.html');
+  res.sendFile('/Users/caso/Desktop/casochatLocal/casochat/index.html');
   console.log('selam');
 });
 
