0c3bcfdb7a9b58951e5e694735cb60b4e8067d3b
Cansu Uludağ <cansu@artistanbulpr.com>
2014-10-31 17:03:36 +0200

    mongodb connection

--- casochat/index.js
+++ casochat/index.js
@@ -2,9 +2,30 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+
+
 app.get('/', function(req, res){
-  res.sendFile('/Users/caso/Desktop/casochatLocal/casochat/index.html');
-  console.log('selam');
+ 	res.sendFile('/Users/caso/Desktop/casochatLocal/casochat/index.html');
+ 	console.log('selam');
+
+	// Connect to the db
+	MongoClient.connect("mongodb://localhost:27017/mydb", function(err, db) {
+	  if(!err) {
+	    console.log("We are connected");
+/*
+	    db.User.find({name: "caso"}, function(err, users) {
+		  if( err || !users) 
+		  	console.log("User can not found");
+		  else 
+		    console.log(users.name);
+		});*/
+	  }
+	});
+
 });
 
 io.on('connection', function(socket){
